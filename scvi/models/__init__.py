a6def05b3cdb7c199a5241773467700e36ee298a
Edouard360 <mailedouardmehlman@gmail.com>
2018-07-03 12:16:06 -0700

    first commit infoCatVAE

--- scvi/models/__init__.py
+++ scvi/models/__init__.py
@@ -1,7 +1,8 @@
 from .svaec import SVAEC
 from .vae import VAE
-from .vaec import VAEC
+from .vaec import VAEC, InfoCatVAEC
 
 __all__ = ['VAEC',
            'SVAEC',
-           'VAE']
+           'VAE',
+           'InfoCatVAEC']
